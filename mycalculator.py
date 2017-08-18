ead3d066e4dffa6c7e8a052e9b9cbe271596a691
blaize21m <30883178+blaize21m@users.noreply.github.com>
2017-08-18 15:31:21 -0400

    Add files via upload

---
+++ mycalculator.py
@@ -0,0 +1,21 @@
+# mycalculator.py 
+
+aa = int(input('Please enter your 1st number: '))
+bb = int(input('Please enter your 2nd number: '))
+
+print ('Please select options from below: \n'
+       'a = addition \n'
+       'b =substruction \n'
+       'c = multiplication \n'
+       'd = division \n')
+
+option = input('You have selected: \n')
+
+if option == 'a':
+    print ('Sum of your 2 numbers are:', int(aa+ bb))
+elif option == 'b':
+    print('Substraction of your 2 numbers are:' , int(aa - bb))
+elif option == 'c':
+    print('Multiplication of your 2 numbers are:' , int(aa * bb))
+elif option == 'd':
+    print('Division of your 2 numbers are:' , float(aa / bb))
